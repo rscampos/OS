e720a35e4879d8fadb8c8ca0983756f9d7f80fd3
Raphael Campos <raphaelcampos.rp@gmail.com>
2015-11-03 23:53:26 -0200

    bochs fix

--- bootloader/stage2/stage2.asm
+++ bootloader/stage2/stage2.asm
@@ -131,6 +131,9 @@ Stage3:
 	mov	esi, KERNELSTART
 	mov	edi, IMAGEPMODE-0x1000          ; because we are using ELF, we
 	cld                                     ; load 1M - 0x1000 for jump to
+        mov     ax, 0x10
+        mov     es, ax
+        mov     ds, ax
 	rep	movsd                           ; 1MB. The first byte is at 1M
 
         ;--------------------------------;
